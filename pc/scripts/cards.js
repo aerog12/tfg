8bc8347bcedf123e10755b68c0cf2967e6ed485f
aerog12 <antoniofb12@gmail.com>
2024-05-10 14:42:44 +0100

    10/05/24 changes

--- pc/scripts/cards.js
+++ pc/scripts/cards.js
@@ -20,3 +20,42 @@ document.addEventListener("DOMContentLoaded", function () {
     card.style.backgroundImage = "url('" + bgImages[index] + "')";
   });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Array de rutas de imágenes
+  var bgImages = [
+    "images/japon/osaka.jpg",
+    "images/eeuu/new york.jpg",
+    "images/londres2.jpg",
+    "images/Roma.jpg"
+  ];
+
+  // Seleccionar todas las tarjetas
+  var cards = document.querySelectorAll(".card-shop");
+
+  // Iterar sobre las tarjetas y asignar una imagen de fondo a cada una
+  cards.forEach(function (card, index) {
+    card.style.backgroundImage = "url('" + bgImages[index] + "')";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Array de rutas de imágenes
+  var bgImages = [
+    "images/comida.jpg",
+    "images/praga.jpg",
+    "images/santorini.jpg",
+    "images/explorar.jpg",
+  ];
+
+  // Seleccionar todas las tarjetas
+  var cards = document.querySelectorAll(".cards-categorias");
+
+  // Iterar sobre las tarjetas y asignar una imagen de fondo a cada una
+  cards.forEach(function (card, index) {
+    card.style.backgroundImage = "url('" + bgImages[index] + "')";
+  });
+});
+
+
