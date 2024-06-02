a80c1636b525d1ea87c413fc1dd4fea7e17db98d
aerog12 <antoniofb12@gmail.com>
2024-06-02 19:22:11 +0100

    commit

--- ordenador/frontend/src/app/login/page.js
+++ ordenador/frontend/src/app/login/page.js
@@ -1,9 +1,33 @@
+'use client';
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Header from "../../../components/header";
 import "/styles/form.css";
 
 export default function Login() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    await fetch("http://localhost:8000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    router.push("/");
+  };
+
   return (
-    //react fragments
     <>
       <Header
         imagenFondo="/images/fondosNavbar/fondoLogin.jpg"
@@ -12,6 +36,52 @@ export default function Login() {
         cuerpoTexto="Inicie sesión para tener acceso a todas las funcionalidades de la pagina"
         estaEnLogin={true}
       />
+
+      <div className="form-container-login">
+        <form id="formularioRegistro" onSubmit={submit}>
+          <div className="mail">
+            <label className="mail-text" htmlFor="email">
+              EMAIL
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="correo"
+              autoFocus
+              placeholder="Introduce tu email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="passwd">
+            <label className="passwd-text" htmlFor="password">
+              CONTRASEÑA
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="***********"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="action-button-form">
+            INICIAR SESIÓN
+          </button>
+          <div className="line-gray"></div>
+          <div className="createAccount">
+            <p className="createAccount-text">¿No tienes cuenta?</p>
+            <a href="/register" className="btn-createAccount">
+              REGISTRATE
+            </a>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
--- ordenador/frontend/src/app/register/page.js
+++ ordenador/frontend/src/app/register/page.js
@@ -1,8 +1,48 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import Header from "../../../components/header";
 import "/styles/headerTextContainer.css";
 import "/styles/form.css";
+import "/styles/navBar.css";
 
 export default function Register() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    await fetch("http://localhost:8000/api/registro", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        email,
+        name,
+        password,
+      }),
+    });
+
+    await fetch("http://localhost:8000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    router.push("/");
+  };
+
   return (
     <>
       <Header
@@ -12,6 +52,64 @@ export default function Register() {
         cuerpoTexto="Cree una nueva cuenta para tener acceso a todas las funcionalidades de la pagina"
         estaEnRegistro={true}
       />
+      <div className="form-container">
+        <form id="formularioRegistro" onSubmit={submit}>
+          <div className="mail">
+            <label className="mail-text" htmlFor="email">
+              EMAIL
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="correo"
+              autoFocus
+              placeholder="Introduce tu email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="name">
+            <label className="mail-text" htmlFor="name">
+              NOMBRE COMPLETO
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="usuario"
+              placeholder="Introduce tu nombre"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="passwd">
+            <label className="passwd-text" htmlFor="password">
+              CONTRASEÑA
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="***********"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="action-button-form">
+            REGISTRATE
+          </button>
+          <div className="line-gray"></div>
+          <div className="createAccount">
+            <p className="createAccount-text">¿Tienes cuenta?</p>
+            <a href="/login" className="btn-createAccount">
+              INICIA SESIÓN
+            </a>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
