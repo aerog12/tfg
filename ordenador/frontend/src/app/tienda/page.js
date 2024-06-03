10aa2fb8c066f469d5cd9d433d0ab45887528d67
aerog12 <antoniofb12@gmail.com>
2024-06-03 17:06:50 +0100

    commit

--- ordenador/frontend/src/app/tienda/page.js
+++ ordenador/frontend/src/app/tienda/page.js
@@ -114,7 +114,7 @@ export default function Tienda() {
             </div>
           </div>
           <div className="card-shop">
-            <div className="text-container">
+            <div className="text-container-tienda">
               <h3 className="text-container-titulo">LONDRES :</h3>
               <h3 className="text-container-titulo-morado">GUÍA COMPLETA</h3>
               <div className="guide-pages">
