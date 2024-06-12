42d4ea570b6631599e5c9ef1bea17a22e4cf7e4d
aerog12 <antoniofb12@gmail.com>
2024-06-12 12:32:44 +0200

    commit

--- ordenador/frontend/src/app/register/page.js
+++ ordenador/frontend/src/app/register/page.js
@@ -67,7 +67,6 @@ export default function Register() {
               type="email"
               id="email"
               name="correo"
-              autoFocus
               placeholder="Introduce tu email"
               required
               value={email}
