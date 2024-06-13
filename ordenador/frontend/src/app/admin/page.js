32c55b77253073640d7d38b2b161fa006af3a80b
aerog12 <antoniofb12@gmail.com>
2024-06-13 12:32:38 +0200

    commit

--- ordenador/frontend/src/app/admin/page.js
+++ ordenador/frontend/src/app/admin/page.js
@@ -7,7 +7,8 @@ export default function Admin() {
   const [informacionAdmin, setInformacionAdmin] = useState(null);
   const sitioLaravel = process.env.NEXT_PUBLIC_SITIO_LARAVEL;
   const [usuarios, setUsuarios] = useState([]);
-  const [selectedUserId, setSelectedUserId] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const usuariosPorPagina = 5;
 
   useEffect(() => {
     const fetchInformacionAdmin = async () => {
@@ -72,38 +73,81 @@ export default function Admin() {
     }
   };
 
+  const indiceUltimoUsuario = currentPage * usuariosPorPagina;
+  const indicePrimerUsuario = indiceUltimoUsuario - usuariosPorPagina;
+  const usuariosActuales = usuarios.slice(
+    indicePrimerUsuario,
+    indiceUltimoUsuario
+  );
+
+  const paginas = [];
+  for (let i = 1; i <= Math.ceil(usuarios.length / usuariosPorPagina); i++) {
+    paginas.push(i);
+  }
+
+  const cambiarPagina = (numeroPagina) => {
+    setCurrentPage(numeroPagina);
+  };
+
   return (
     <>
       <Header
         imagenFondo="/images/londres2.jpg"
-        // isAuthenticated={estaLogueado}
         texto1={<span className="texto-con-letter-spacing">¡ BIENVENID@ </span>}
         texto2={
           <span className="texto-con-letter-spacing2">
-            {/* {`${informacionUsuario?.name}`} <span> !</span> */}
+            {informacionAdmin?.email} <span>!</span>
           </span>
         }
         cuerpoTexto="¡Estas a punto de comenzar tu próxima aventura! Explora el mundo con nosotros y descubre destinos inolvidables. 🌍✈️"
       />
       <div className="container-admin">
-        <h1>Hola {informacionAdmin?.email}</h1>
-        <h2>Lista de Usuarios:</h2>
-
-        <ul>
-          {usuarios
-            .filter((usuario) => usuario.email !== informacionAdmin?.email) // Filtrar el administrador
-            .map((usuario) => (
-              <li key={usuario.id}>
-                {usuario.name} - {usuario.apellido1} - {usuario.apellido2} - -{" "}
-                {usuario.movil} - {usuario.email} -
-                {informacionAdmin?.email === "admin@admin" && (
-                  <button onClick={() => eliminarUsuario(usuario.id)}>
-                    Eliminar
-                  </button>
-                )}
-              </li>
+        <div className="background-table">
+          <h2>Lista de Usuarios:</h2>
+          <table className="user-table">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Apellido 1</th>
+                <th>Apellido 2</th>
+                <th>Email</th>
+                <th>Móvil</th>
+                {informacionAdmin?.email === "admin@admin" && <th>Acciones</th>}
+              </tr>
+            </thead>
+            <tbody>
+              {usuariosActuales
+                .filter((usuario) => usuario.email !== informacionAdmin?.email)
+                .map((usuario) => (
+                  <tr key={usuario.id}>
+                    <td>{usuario.name}</td>
+                    <td>{usuario.apellido1}</td>
+                    <td>{usuario.apellido2}</td>
+                    <td>{usuario.email}</td>
+                    <td>{usuario.movil}</td>
+                    {informacionAdmin?.email === "admin@admin" && (
+                      <td>
+                        <button onClick={() => eliminarUsuario(usuario.id)}>
+                          Eliminar
+                        </button>
+                      </td>
+                    )}
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          <div className="pagination">
+            {paginas.map((numero) => (
+              <button
+                key={numero}
+                onClick={() => cambiarPagina(numero)}
+                className={currentPage === numero ? "active" : ""}
+              >
+                {numero}
+              </button>
             ))}
-        </ul>
+          </div>
+        </div>
       </div>
     </>
   );
